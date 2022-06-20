4d81b823a56c6390b8d0f22cb49cdaca9032c39d
Wojciech Rzóska <wojciechrzoska@gmail.com>
2022-06-20 16:46:46 +0200

    add game mechanics

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const { Engine, Render, Runner, Bodies, World } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 6;
-const width = 600;
-const height = 600;
+const cellsHorizontal = 10;
+const cellsVertical = 7;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
     wireframes: true,
-    width: width,
-    height: height,
+    width,
+    height,
   },
 });
 
@@ -49,20 +52,20 @@ const shuffle = (arr) => {
   return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false));
+  .map(() => Array(cellsHorizontal - 1).fill(false));
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const stepThroughCell = (row, column) => {
   //if I visited that cell then return
@@ -86,9 +89,9 @@ const stepThroughCell = (row, column) => {
     //check if next cell is not out of border
     if (
       nextRow < 0 ||
-      nextRow >= cells ||
+      nextRow >= cellsVertical ||
       nextColumn < 0 ||
-      nextColumn >= cells
+      nextColumn >= cellsHorizontal
     ) {
       continue;
     }
@@ -120,11 +123,12 @@ horizontals.forEach((row, rowIndex) => {
       return;
     }
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
-      10,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
+      5,
       {
+        label: 'wall',
         isStatic: true,
       }
     );
@@ -138,14 +142,80 @@ verticals.forEach((row, rowIndex) => {
       return;
     }
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
-      10,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
+      5,
+      unitLengthY,
       {
+        label: 'wall',
         isStatic: true,
       }
     );
     World.add(world, wall);
   });
+});
+
+//create end point
+const goal = Bodies.rectangle(
+  width - unitLengthX / 2,
+  height - unitLengthY / 2,
+  unitLengthX * 0.6,
+  unitLengthY * 0.6,
+  {
+    isStatic: true,
+    label: 'goal',
+  }
+);
+
+World.add(world, goal);
+
+//create player ball
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
+const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
+  label: 'ball',
+});
+
+World.add(world, ball);
+
+document.addEventListener('keydown', (e) => {
+  const { x, y } = ball.velocity;
+
+  if (e.key === 'w') {
+    Body.setVelocity(ball, { x, y: y - 1 });
+  }
+
+  if (e.key === 's') {
+    Body.setVelocity(ball, { x, y: y + 1 });
+  }
+
+  if (e.key === 'a') {
+    Body.setVelocity(ball, { x: x - 1, y });
+  }
+
+  if (e.key === 'd') {
+    Body.setVelocity(ball, { x: x + 1, y });
+  }
+});
+
+//win
+Events.on(engine, 'collisionStart', (e) => {
+  for (let collision of e.pairs) {
+    const labels = ['ball', 'goal'];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y = 1;
+      for (let item of world.bodies) {
+        if (item.label === 'wall') {
+          Body.setStatic(item, false);
+        }
+      }
+      World.remove(world, ball);
+      World.remove(world, goal);
+    } else {
+      Body.setPosition(ball, { x: unitLengthX / 2, y: unitLengthY / 2 });
+    }
+  }
 });
